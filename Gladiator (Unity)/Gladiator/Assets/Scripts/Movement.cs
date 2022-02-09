61ab1581d200dffd9aacaba85d7f344efc168214
Bennie18016 <18016@totton.ac.uk>
2022-02-09 19:07:33 +0000

    Update 1.10
    Added sprinting and jumping to the code

--- Gladiator (Unity)/Gladiator/Assets/Scripts/Movement.cs
+++ Gladiator (Unity)/Gladiator/Assets/Scripts/Movement.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class Movement : MonoBehaviour
 {
-    //Creating important variables
+    //Creating important variables. Serialize field lets private values be shown in the inspector
     float speed = 6f;
-    [SerializeField] Camera playercamera;
-    float sensitivty = 2f;
+    [SerializeField] Camera playerCamera;
+    float sprint = 8.5f;
+    float jumpHeight = 8.0f;
+    float sensitivity = 2.0f;
     float gravity = 20.0f;
-    float xlimt = 45.00f;
+    float xlimit = 45.0f;
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
@@ -24,34 +28,56 @@ public class Movement : MonoBehaviour
         Cursor.visible = false;
     }
 
-    private void Update()
+    void Update()
     {
         //Makes the direction we can move. This is to stop us from being able to fly upwards if we ae looking into the sky and holding W
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        //Creates the amount we are going to move by.
-        float curSpeedX = speed * Input.GetAxis("Vertical");
-        float curSpeedY = speed * Input.GetAxis("Horizontal");
-        //Part one of moving, 
+        //Hold LShift to sprint
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        //Creates the speed that we move at. The ? will pick the output depending on if the boolean is true or false. The first value (in this case "sprint" would output if it is true and "speed" will output if it is false
+        float curSpeedX = (running ? sprint : speed) * Input.GetAxis("Vertical");
+        float curSpeedY = (running ? sprint : speed) * Input.GetAxis("Horizontal");
+
+        //Sets the value of "movementDirectionY" to whatever the y value of the player is
+        float movementDirectionY = moveDirection.y;
+        //Part one of moving. It multiplies the speed of X and Y with the Z and X value
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        //If spacebar is pressed and the character is on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+        {
+            //Move us in the direction of y by the height we can jump
+            moveDirection.y = jumpHeight;
+        }
+        else
+        {
+            //Resets the value
+            moveDirection.y = movementDirectionY;
+        }
 
         //Checks if we are in the air or on the ground
         if (!characterController.isGrounded)
         {
             //Moves us to the ground if we are in the air
             moveDirection.y -= gravity * Time.deltaTime;
+
+            /*
+             * When I discovered this, I prefered it over using tags
+             * This is because its much simplier to write and altogether
+             * Makes the code look a LOT cleaner
+             */
         }
 
         //Actually moves us
         characterController.Move(moveDirection * Time.deltaTime);
 
         //Moves our camera where our mouse goes
-        rotationX += -Input.GetAxis("Mouse Y") * sensitivty;
-        //Stops us from doing barrel rolls
-        rotationX = Mathf.Clamp(rotationX, -xlimt, xlimt);
-        playercamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensitivty, 0);
+        rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
+        rotationX = Mathf.Clamp(rotationX, -xlimit, xlimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensitivity, 0);
     }
-
 }
